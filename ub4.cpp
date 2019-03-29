624463adc21086dfcb62055ccb090751a077b310
3a713214 <48340352+3a713214@users.noreply.github.com>
2019-03-29 21:40:21 +0800

    Add files via upload

---
+++ ub4.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+int main(void)
+{
+   int sec=0; 
+    printf("請輸入秒數：");
+    scanf("%d",&sec); 
+    printf("轉換的時間為=%02d時:%02d分:%02d秒\n",sec/3600%60,sec/60%60,sec%60); 
+ 	system("pause"); 
+ 	return 0; 
+}
+    
+
+
+
